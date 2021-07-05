954e7b8aaded16d7eb03da3ea188cc799a7d7c20
ADMIN <ADMIN@DESKTOP-BN1GJNU>
2021-07-05 11:13:57 +0530

    FirstCommit Upload all files

---
+++ PracticeJava/Abstract.java
@@ -0,0 +1,21 @@
+
+
+public class Abstract
+{
+	int sno;
+	String sName;
+	
+	public static void main(String[] args) 
+	
+	{
+		Abstract s1= new Abstract();
+		s1.sno=101;
+		s1.sName="anil";
+		
+		Abstract s2= new Abstract();
+		s2.sno=102;
+		s2.sName="sunil";
+		
+	}
+
+}
